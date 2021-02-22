d91252a479a794ce7fd068f08b5ef7445d0d1d1c
Ante Sepic <antesepic@protonmail.com>
2021-02-23 00:29:22 +0100

    :fire: Remove unused imports

--- src/features/auth/server.ts
+++ src/features/auth/server.ts
@@ -1,11 +1,6 @@
-import catchify from 'catchify';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import auth0 from '@/src/features/auth/auth0';
-
-interface AccessTokenResponseInterface {
-  accessToken?: string | undefined;
-}
 
 export const getAccessToken = async (
   req: NextApiRequest,
