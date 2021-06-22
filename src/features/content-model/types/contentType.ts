9df5fe4f3ddc44fac2ff94e5501a2963316ad58c
Ante Sepic <antesepic@protonmail.com>
2021-06-22 22:08:47 +0200

    :bug: Fix content model schema

--- src/features/content-model/types/contentType.ts
+++ src/features/content-model/types/contentType.ts
@@ -6,7 +6,7 @@ import sysSchema from '@/src/features/content-model/types/sys';
 const contentTypeSchema = z.object({
   sys: sysSchema,
   name: z.string(),
-  displayField: z.string(),
+  displayField: z.nullable(z.string()),
   description: z.nullable(z.string()),
   fields: z.array(contentTypeFieldSchema),
   internal: z.optional(z.boolean()),
