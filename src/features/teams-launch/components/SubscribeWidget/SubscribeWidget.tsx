feac4f9588f7d674d4917a859fa8164266473269
Ante Sepic <antesepic@protonmail.com>
2024-11-03 17:44:34 +0100

    Update SubscribeWidget.tsx

--- src/features/teams-launch/components/SubscribeWidget/SubscribeWidget.tsx
+++ src/features/teams-launch/components/SubscribeWidget/SubscribeWidget.tsx
@@ -12,7 +12,8 @@ const SubscribeWidget: React.FC = () => {
       }}>A new and updated version of ContentModel.io is coming with many exciting new features 👀</p>
       <>
         <iframe data-w-token="42d8a83fcc9e255da714" data-w-type="pop-in" frameBorder="0" scrolling="yes" marginHeight={0} marginWidth={0} src="https://x7yh9.mjt.lu/wgt/x7yh9/l1p/form?c=d7532988" width="100%" style={{ height: 0 }} />
-    <iframe data-w-token="42d8a83fcc9e255da714" data-w-type="trigger" frameBorder="0" scrolling="no" marginHeight={0} marginWidth={0} src="https://x7yh9.mjt.lu/wgt/x7yh9/l1p/trigger?c=4a2d09a1" width="100%" style={{ height: 0 }} />
+        <iframe data-w-token="42d8a83fcc9e255da714" data-w-type="trigger" frameBorder="0" scrolling="no" marginHeight={0} marginWidth={0} src="https://x7yh9.mjt.lu/wgt/x7yh9/l1p/trigger?c=4a2d09a1" width="100%" style={{ height: 0 }} />
+      </>
     </div>
   );
 };
